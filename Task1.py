f33104db268ecae8ad99194a5dbedfe0f6309c77
Alex Kizilov <as_k@list.ru>
2022-12-18 11:23:44 +0400

    Task1 finished

---
+++ Task1.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
+
+def GetSum(num):
+    sum = 0     
+    my_list = num.split('.')
+    for i in range(len(my_list)):
+        for j in range(len(my_list[i])):
+            sum = sum + int(my_list[i][j])
+    return sum    
+n = input('Введите число: ')
+res = GetSum(n)
+print(res)
